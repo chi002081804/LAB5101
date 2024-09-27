119073029ced8d1cbb1cb95122422995acc3dd56
NoElectricityPika <okchizhang@gmail.com>
2024-09-27 13:23:14 -0400

    optimize the structure

--- Lab_3_Navigation_flow_in_Java_Swing/src/main/java/com/chi/infolab/Main.java
+++
@@ -1,7 +0,0 @@
-package com.chi.infolab;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- out/production/LAB5101/README.md
+++
@@ -1,2 +0,0 @@
-# Hello-World
-LAB 5101 Hello World
